523c517d719638aa99f8b1b36ad55e57272882b8
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-06-01 16:48:49 +0530

    fixed content

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
       <div className="main-logo">
         <img src="https://msit.in/static/img/msit.png" alt="MSIT Logo" />
       </div>
-          <div className="content">
-              <h1>Maharaja Surajmal Institue of Technology</h1>
-              <span className="sub-heading">Affiliated to GGSIPU | NAAC Accredited 'A' Grade | NBA (CSE, IT, ECE,EEE) | Approved by AICTE | ISO 9001:2015 Certified</span>
+      <div className="content">
+        <h1>Maharaja Surajmal Institute Of Technology</h1>
+        <span className="sub-heading">Affiliated to GGSIPU | NAAC Accredited 'A' Grade | NBA (CSE, IT, ECE,EEE) | Approved by AICTE | ISO 9001:2015 Certified</span>
       </div>
       {/* <div className="back-logo">
         <img className="g20" src="/images/g20.jpg" alt="G20 Logo" />
