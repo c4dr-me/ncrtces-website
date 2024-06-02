48b599ec2354010218626945f73499c1be672943
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-06-02 15:18:07 +0530

    added responsiveness

--- components/Main.jsx
+++ components/Main.jsx
@@ -3,7 +3,37 @@ import React from 'react'
 const Main = () => {
   return (
     <div>
-      <img src='./college_blur_img.jpg'/>
+      <img src='./college_blur_img.jpg' />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sapiente harum nam eos totam esse tenetur sint asperiores accusamus doloremque, ad officia reiciendis? Nam laudantium sit, debitis corrupti voluptatum soluta.
+      Quisquam nihil praesentium, similique temporibus repellendus cum architecto possimus tempore reprehenderit, quis blanditiis vero aut ratione eligendi dolorum porro quasi corrupti accusantium nostrum labore ducimus placeat! Perspiciatis ipsum modi sint.
+      Accusamus sequi minima quisquam id consectetur atque officia voluptatibus vero, modi ratione. Temporibus, soluta sequi quod aspernatur eos iusto ullam accusamus fuga sunt unde distinctio adipisci mollitia perferendis dolores amet.
+      Accusantium dicta, tenetur, in dolores ea voluptatem porro earum provident facilis natus corrupti dolorem magni voluptatum aperiam eum pariatur, delectus consequuntur ipsam fugiat. Corporis aliquam recusandae quo nulla consequuntur aperiam.
+      Debitis, explicabo quam? Qui nisi deleniti laborum quo quod iure neque, illo reiciendis iste? Eius delectus vitae a nihil cumque, distinctio, libero expedita perspiciatis quam ad, fuga praesentium dolorem deserunt.
+      Minima quos provident architecto officiis quo laboriosam rem, eos amet, quam, quod illo assumenda! Nesciunt hic obcaecati itaque adipisci officiis magni commodi magnam vitae, architecto repellendus fugiat, necessitatibus voluptate quidem!
+      Consequuntur esse corrupti libero rem. Dolores eum ipsam dolor voluptas impedit veritatis aspernatur similique, voluptatem, itaque iure debitis maxime blanditiis nisi vitae suscipit cupiditate non obcaecati beatae incidunt aut! Perferendis.
+      Sapiente ex natus quas, voluptatem fuga fugiat reiciendis ad magnam tempora delectus itaque consectetur adipisci corrupti dolorum quibusdam explicabo suscipit necessitatibus laudantium dolor reprehenderit quaerat labore officiis. Eius, maiores nulla?
+      Obcaecati, explicabo. Aliquam iusto maxime rerum delectus consequatur nobis facilis necessitatibus ad explicabo incidunt esse voluptatem dolorem ea itaque repudiandae eos doloremque vitae exercitationem, illo nam rem? Iste, aperiam minus!
+      Explicabo quae hic nesciunt quidem dolorum, magnam impedit sapiente, eaque odit atque doloremque? Dolorem, et minus velit numquam sunt similique iste, laborum deleniti beatae ducimus, hic eligendi magnam vitae? Officia?
+      Tempore consequatur quis suscipit nobis recusandae animi magnam iusto! Recusandae vitae iure ab doloremque deserunt quibusdam maiores porro necessitatibus veniam eos rem possimus eligendi consequuntur, obcaecati excepturi! Rem, vero beatae!
+      Sequi, expedita consequatur omnis ipsum quae ullam quo ea error! Esse omnis, nostrum repellendus quibusdam officiis sit itaque cumque alias assumenda, id aspernatur. Veritatis recusandae vel inventore quia, modi ipsum.
+      Exercitationem nihil fuga quas delectus ipsum laborum ratione expedita adipisci voluptates ex non obcaecati, quia, maiores quidem totam! Odit accusamus consequuntur vel, praesentium cum officiis est esse ducimus dolor aliquid?
+      Error facilis vero laborum minima sequi, quibusdam minus. Aperiam explicabo ratione eligendi ab commodi dolorem quasi, deleniti possimus minus, consequuntur fuga quisquam enim illum voluptatibus dignissimos temporibus? Commodi, cupiditate repellat.
+      Amet aperiam corporis ad nemo eius tempora, autem aspernatur fugiat est totam facilis laboriosam recusandae aliquid minima. Quia cumque harum voluptatum incidunt distinctio facere natus, dicta eligendi aliquam! Labore, obcaecati.
+      Numquam, dolor distinctio porro qui reprehenderit fugiat cum ipsum corporis atque placeat fugit, rem aut minus repellat sed quod iste delectus cumque. Eveniet inventore adipisci impedit saepe soluta, excepturi ratione?
+      Distinctio, explicabo unde non saepe iste quae est cumque quod illum commodi! Sapiente, repudiandae. Sed enim tempore fuga facere aliquam voluptatum excepturi! Aut harum nihil fugit at, doloremque provident officiis.
+      Vero ad optio explicabo iste dolore alias rem excepturi odio, accusamus, cum ipsum sint sed inventore, maxime iure voluptate dolorem iusto reiciendis veritatis obcaecati aperiam. Voluptatibus, saepe sapiente! Possimus, consectetur.
+      Assumenda, unde quis, libero totam nemo voluptatum fuga sed enim, ab veniam eaque? Voluptatibus ratione molestias rem placeat quos? Quaerat ratione id et esse nisi voluptate pariatur ad commodi dolorem!
+      Quia explicabo ducimus eveniet non hic commodi esse! Provident, nemo. Corporis cumque esse, nobis optio cum fuga. Explicabo, maiores totam omnis cumque vel laborum unde necessitatibus nihil suscipit! Ducimus, fugit.
+      Reprehenderit voluptatum numquam aut aperiam, non illum eaque mollitia soluta quibusdam deleniti tenetur recusandae atque? Repudiandae perspiciatis maxime dolorem nesciunt delectus tempora officiis exercitationem quam dolores consectetur. Quo, dicta libero!
+      Id laborum facere doloremque maxime repellat quaerat totam porro consectetur libero quibusdam, voluptas consequuntur nam, saepe odio recusandae similique, maiores minima commodi officiis necessitatibus magni soluta unde ratione possimus? Aliquam.
+      Quasi suscipit cupiditate vero eveniet quo? Totam adipisci quis dolorum maiores sequi, illo quo nesciunt accusantium itaque sint numquam, tempora error blanditiis ratione. Deserunt libero facere, eius amet quis laborum!
+      Aliquid aliquam deserunt reiciendis fuga adipisci a laborum cupiditate animi reprehenderit quasi delectus iusto, et, facilis exercitationem quas sint impedit magnam consectetur eum accusamus. Iure expedita eligendi reiciendis nisi magni.
+      Iure cupiditate eius nisi tempore voluptatem fugit, obcaecati at nostrum libero a non perspiciatis sequi praesentium esse exercitationem numquam, voluptatibus saepe. Ipsa labore porro soluta nemo ad exercitationem temporibus harum.
+      Qui quis possimus recusandae omnis dolores sequi, deleniti facere ex, dolore magni assumenda perferendis necessitatibus eos amet voluptatem nulla corporis beatae, consequuntur expedita? Ea minima vitae eum ipsum, sed nesciunt.
+      Inventore, eius mollitia! Quidem, quod laboriosam porro quos, ab quia laborum laudantium excepturi eius voluptates pariatur tempore recusandae odit soluta earum iusto! Impedit, suscipit quam est quaerat ea laborum perferendis.
+      Cum unde perspiciatis natus repellat assumenda enim? Commodi, atque. Corporis eaque obcaecati laudantium sit, ducimus architecto magnam rerum facere ad ipsam labore, placeat recusandae odio reiciendis laborum natus iure quaerat?
+      Distinctio esse suscipit possimus voluptatibus ea nemo aliquid rem hic! Omnis id, nihil asperiores quidem maiores neque aliquid enim saepe praesentium quam vel facilis placeat et vero numquam veniam nesciunt.
+      Ad nobis eveniet laboriosam est voluptatibus! Asperiores amet quos commodi delectus ipsum modi cumque quod dolore, accusantium ullam deserunt, sapiente quidem officiis cupiditate ad praesentium maxime rem nihil. Qui, corporis.</p>
     </div>
   )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,13 +3,17 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
-import { Link } from "react-router-dom";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { Link, useLocation } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const pages = [
   { name: "Overview", path: "/" },
@@ -23,22 +27,46 @@ const pages = [
 ];
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [menuOpen, setMenuOpen] = React.useState(false);
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+  const [marginTop, setMarginTop] = React.useState('0px');
+  const location = useLocation();
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-    setMenuOpen(true);
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.up('sm'));
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-    setMenuOpen(false);
+  const handleCloseDrawer = () => {
+    setDrawerOpen(false);
   };
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      const navbar = document.querySelector('#appbar');
+      if (navbar) {
+        const navbarRect = navbar.getBoundingClientRect();
+        if (navbarRect.top <= 0) {
+          setMarginTop('3.5rem');
+        } else {
+          setMarginTop(`${navbarRect.bottom}px`);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Initial check
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <AppBar
-      position="static"
+      position="sticky"
+      id="appbar"
       sx={{
         background:
           "linear-gradient(162deg, rgba(75,67,193,0.9809173669467787) 0%, rgba(58,58,194,1) 32%, rgba(58,110,195,1) 56%)",
@@ -53,61 +81,67 @@ function ResponsiveAppBar() {
             sx={{
               flexGrow: 1,
               display: { xs: "flex", md: "none" },
+              alignItems: "center"
             }}
           >
             <IconButton
               size="large"
               aria-label="menu"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
+              onClick={handleDrawerToggle}
               color="inherit"
+              sx={{ position: 'absolute', zIndex: 1300 }}
             >
-              <MenuIcon />
+              {drawerOpen ? <CloseIcon sx={{ fontSize: 32 }} /> : <MenuIcon sx={{ fontSize: 32 }} />}
             </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              open={menuOpen}
-              onClose={handleCloseNavMenu}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
+            <Drawer
+              anchor="left"
+              open={drawerOpen}
+              onClose={handleCloseDrawer}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
               }}
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              getContentAnchorEl={null}
-              elevation={10}
               PaperProps={{
-                sx: { width: 200 }, // Set the desired width here
+                sx: {
+                  width: '250px',
+                  mt: marginTop,
+                  ...(isSmallScreen && {
+                    mt: '4rem'
+                  }),
+                }
               }}
             >
-              {pages.map((page) => (
-                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
-                  <Typography
-                    textAlign="center"
-                    sx={{
-                      fontFamily: "inherit",
-                      background:
-                        "linear-gradient(162deg, rgba(75,67,193,0.9809173669467787) 0%, rgba(58,58,194,1) 32%, rgba(58,110,195,1) 56%)",
-                      backdropFilter: "blur(10px)",
-                      boxShadow: "none",
-                      WebkitBackgroundClip: "text",
-                      WebkitTextFillColor: "transparent",
-                    }}
-                  >
-                    <Link
+              <Box
+                sx={{
+                  width: 250,
+                  background:
+                    "linear-gradient(162deg, rgba(75,67,193,0.9809173669467787) 0%, rgba(58,58,194,1) 32%, rgba(58,110,195,1) 56%)",
+                  height: "100%",
+                  color: "white",
+                }}
+                role="presentation"
+                onClick={handleCloseDrawer}
+                onKeyDown={handleCloseDrawer}
+              >
+                <List>
+                  {pages.map((page) => (
+                    <ListItem
+                      key={page.name}
+                      component={Link}
                       to={page.path}
-                      style={{ textDecoration: "none", color: "inherit" }}
+                      sx={{
+                        color: "white",
+                        backgroundColor: location.pathname === page.path ? "rgba(255, 255, 255, 0.2)" : "transparent",
+                        "&:hover": {
+                          backgroundColor: "rgba(255, 255, 255, 0.1)",
+                        },
+                      }}
                     >
-                      {page.name}
-                    </Link>
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+                      <ListItemText primary={page.name} />
+                    </ListItem>
+                  ))}
+                </List>
+              </Box>
+            </Drawer>
           </Box>
           <Box
             sx={{
@@ -119,6 +153,8 @@ function ResponsiveAppBar() {
             {pages.map((page) => (
               <Button
                 key={page.name}
+                component={Link}
+                to={page.path}
                 sx={{
                   my: 2,
                   color: "white",
@@ -129,8 +165,6 @@ function ResponsiveAppBar() {
                     backgroundColor: "rgba(255, 255, 255, 0.2)",
                   },
                 }}
-                component={Link}
-                to={page.path}
               >
                 {page.name}
               </Button>
