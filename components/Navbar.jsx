b50e1552bfdee117472d60d51d9b241a4dc19185
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-06-06 22:44:30 +0530

    fixed the sidebar bug

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,8 +3,8 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Drawer from "@mui/material/Drawer";
@@ -34,14 +34,14 @@ const rotateOpen = keyframes`
  
   }
   to {
-    transform: rotate(90deg);
+    transform: rotate(180deg);
 
   }
 `;
 
 const rotateClose = keyframes`
   from {
-    transform: rotate(90deg);
+    transform: rotate(180deg);
 
   }
   to {
@@ -52,18 +52,21 @@ const rotateClose = keyframes`
 
 function ResponsiveAppBar() {
   const [drawerOpen, setDrawerOpen] = React.useState(false);
-  const [marginTop, setMarginTop] = React.useState('0px');
+  const [marginTop, setMarginTop] = React.useState("0px");
+  const [isNavbarAtTop, setIsNavbarAtTop] = React.useState(false);
+
+
   const location = useLocation();
 
   const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
-  const isMediumScreen = useMediaQuery(theme.breakpoints.between('sm', 'md'));
-  const isLargeScreen = useMediaQuery(theme.breakpoints.up('md'));
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMediumScreen = useMediaQuery(theme.breakpoints.between("sm", "md"));
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up("md"));
 
   const handleDrawerToggle = () => {
     setDrawerOpen(!drawerOpen);
     if (!drawerOpen) {
-      window.scrollTo({ top: 400, behavior: "smooth" });
+      window.scrollTo({ top: 300, behavior: "smooth" });
     }
   };
 
@@ -73,27 +76,28 @@ function ResponsiveAppBar() {
 
   React.useEffect(() => {
     const handleScroll = () => {
-      const navbar = document.querySelector('#appbar');
+      const navbar = document.querySelector("#appbar");
       if (navbar) {
         const navbarRect = navbar.getBoundingClientRect();
         if (navbarRect.top <= 0) {
+          setIsNavbarAtTop(true);
           if (isMediumScreen) {
-            setMarginTop('4rem');
+            setMarginTop("4rem");
           } else {
-            setMarginTop('3.5rem');
+            setMarginTop("3.4rem");
           }
         } else {
+          setIsNavbarAtTop(false);
           setMarginTop(`${navbarRect.bottom}px`);
         }
       }
     };
 
-
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     handleScroll(); // Initial check
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, [isMediumScreen, isSmallScreen]);
 
@@ -102,6 +106,8 @@ function ResponsiveAppBar() {
       setDrawerOpen(false);
     }
   }, [isLargeScreen, drawerOpen]);
+
+  
 
   return (
     <AppBar
@@ -121,7 +127,7 @@ function ResponsiveAppBar() {
             sx={{
               flexGrow: 1,
               display: { xs: "flex", md: "none" },
-              alignItems: "center"
+              alignItems: "center",
             }}
           >
             <IconButton
@@ -130,62 +136,72 @@ function ResponsiveAppBar() {
               onClick={handleDrawerToggle}
               color="inherit"
               sx={{
-                position: 'absolute',
+                position: "absolute",
                 zIndex: 1300,
-                animation: drawerOpen ? `${rotateOpen} 200ms ease-in forwards` : `${rotateClose} 200ms ease-in forwards;`,
+                animation: drawerOpen
+                  ? `${rotateOpen} 200ms ease-in forwards`
+                  : `${rotateClose} 200ms ease-in forwards;`,
               }}
             >
-              {drawerOpen ? <CloseIcon sx={{ fontSize: 32 }} /> : <MenuIcon sx={{ fontSize: 32 }} />}
+              {drawerOpen ? (
+                <CloseIcon sx={{ fontSize: 32 }} />
+              ) : (
+                <MenuIcon sx={{ fontSize: 32 }} />
+              )}
             </IconButton>
-            <Drawer
-              anchor="left"
-              open={drawerOpen}
-              onClose={handleCloseDrawer}
-              ModalProps={{
-                keepMounted: true, // Better open performance on mobile.
-              }}
-              PaperProps={{
-                sx: {
-                  width: '15.625rem',
-                  mt: marginTop,
-                  overflow: 'auto',
-                  height: '100vh',
-                }
-              }}
-            >
-              <Box
-                sx={{
-                  width: 250,
-                  background:
-                    "linear-gradient(162deg, rgba(75,67,193,0.9809173669467787) 0%, rgba(58,58,194,1) 32%, rgba(58,110,195,1) 56%)",
-                  height: "100%",
-                  color: "white",
-                  overflorY: "auto",
+            {isNavbarAtTop && (
+              <Drawer
+                anchor="left"
+                open={drawerOpen}
+                onClose={handleCloseDrawer}
+                ModalProps={{
+                  keepMounted: true, // Better open performance on mobile.
                 }}
-                role="presentation"
-                onClick={handleCloseDrawer}
-                onKeyDown={handleCloseDrawer}
+                PaperProps={{
+                  sx: {
+                    width: "15.625rem",
+                    mt: marginTop,
+                    height: "100%",
+                  },
+                }}
               >
-                <List>
-                  {pages.map((page) => (
-                    <ListItem
-                      key={page.name}
-                      component={Link}
-                      to={page.path}
-                      sx={{
-                        color: "white",
-                        backgroundColor: location.pathname === page.path ? "rgba(255, 255, 255, 0.2)" : "transparent",
-                        "&:hover": {
-                          backgroundColor: "rgba(255, 255, 255, 0.1)",
-                        },
-                      }}
-                    >
-                      <ListItemText primary={page.name} />
-                    </ListItem>
-                  ))}
-                </List>
-              </Box>
-            </Drawer>
+                <Box
+                  sx={{
+                    width: 250,
+                    background:
+                      "linear-gradient(162deg, rgba(75,67,193,0.9809173669467787) 0%, rgba(58,58,194,1) 32%, rgba(58,110,195,1) 56%)",
+                    height: "100%",
+                    color: "white",
+                    overflowY: "auto",
+                  }}
+                  role="presentation"
+                  onClick={handleCloseDrawer}
+                  onKeyDown={handleCloseDrawer}
+                >
+                  <List>
+                    {pages.map((page) => (
+                      <ListItem
+                        key={page.name}
+                        component={Link}
+                        to={page.path}
+                        sx={{
+                          color: "white",
+                          backgroundColor:
+                            location.pathname === page.path
+                              ? "rgba(255, 255, 255, 0.2)"
+                              : "transparent",
+                          "&:hover": {
+                            backgroundColor: "rgba(255, 255, 255, 0.1)",
+                          },
+                        }}
+                      >
+                        <ListItemText primary={page.name} />
+                      </ListItem>
+                    ))}
+                  </List>
+                </Box>
+              </Drawer>
+            )}
           </Box>
           <Box
             sx={{
--- pages/Patron.jsx
+++ pages/Patron.jsx
@@ -14,7 +14,9 @@ const Patrons = () => {
                 <li className="px-3 py-2"><strong> Prof. Tejbir Singh Rana,(Chairman, Academic Committee)</strong> Surajmal Memorial Education Society, New Delhi</li>
                 <li className="px-3 py-2"><strong> Sh. S.S. Solanki,(Chairman Disciplinary(L))</strong></li>
             </ul>
-
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi odio placeat vel, exercitationem, quam similique veritatis perferendis eaque perspiciatis impedit nesciunt dolorum laudantium ducimus ex id explicabo cumque aut. Omnis?
+            Error, possimus quisquam. Nesciunt suscipit delectus alias recusandae, eum veritatis cupiditate asperiores neque quae perspiciatis. Distinctio earum quis exercitationem odit optio nostrum corrupti veniam illum molestias! Laboriosam ab ipsam molestias.
+            Nesciunt explicabo vel eaque! Laboriosam deserunt voluptatem, amet velit reiciendis eaque cumque quae nemo, doloremque cupiditate provident alias. Alias, non pariatur. Magni nulla ipsum tenetur, eveniet tempora est incidunt error.</p>
         </div>
     )
 }
